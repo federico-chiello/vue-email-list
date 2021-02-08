e2fbb93834907efa1e5e020adedfdee10fadac1b
Fede <chiellofederico@gmail.com>
2021-02-08 15:51:38 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 // Genera 10 mail tramite api e stampale in una lista.
-// Potete utilizzare questa api:
-// https://flynn.boolean.careers/exercises/api/random/mail
+
+var app = new Vue({
+  el: '#mail',
+  data: {
+    email: []
+  },
+  mounted(){
+    for (var i = 0; i < 10; i++) {
+      axios
+      .get('https://flynn.boolean.careers/exercises/api/random/mail')
+      .then((result) => {
+        this.email.push(result.data.response)
+        // console.log(this.email);
+      });
+    }
+  }
+});
